c7123447c3b84ae0c28c8963611631408d03b837
loi <dailoi280702@gmail.com>
2022-04-13 19:42:36 +0700

    add coroutines dependencies

--- app/src/main/java/com/example/plantcare/data/repository/LoginSignupRepositoryImpl.kt
+++ app/src/main/java/com/example/plantcare/data/repository/LoginSignupRepositoryImpl.kt
@@ -3,9 +3,8 @@ package com.example.plantcare.data.repository
 import com.example.plantcare.domain.repository.LoginSignupRepository
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.coroutines.CancellationException
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.*
+import kotlinx.coroutines.tasks.await
 import kotlin.coroutines.resumeWithException
 
 class LoginSignupRepositoryImpl(
@@ -16,11 +15,15 @@ class LoginSignupRepositoryImpl(
     email: String,
     password: String
   ) {
-    auth.createUserWithEmailAndPassword(email, password).await()
+    withContext(Dispatchers.IO) {
+      auth.createUserWithEmailAndPassword(email, password).await()
+    }
   }
 
   override suspend fun loginWithEmailAndPassword(email: String, password: String) {
-    auth.signInWithEmailAndPassword(email, password).await()
+    withContext(Dispatchers.IO) {
+      auth.signInWithEmailAndPassword(email, password).await()
+    }
   }
 
   override suspend fun loginWithGoogle() {
@@ -37,37 +40,5 @@ class LoginSignupRepositoryImpl(
 
   override suspend fun logout() {
     auth.signOut()
-  }
-}
-
-@OptIn(ExperimentalCoroutinesApi::class)
-suspend fun <T> Task<T>.await(): T {
-  // fast path
-  if (isComplete) {
-    val e = exception
-    return if (e == null) {
-      if (isCanceled) {
-        throw CancellationException(
-          "Task $this was cancelled normally."
-        )
-      } else {
-        result
-      }
-    } else {
-      throw e
-    }
-  }
-
-  return suspendCancellableCoroutine { cont ->
-    addOnCompleteListener {
-      val e = exception
-      if (e == null) {
-        if (isCanceled) cont.cancel() else cont.resume(result) {
-          throw CancellationException("Task canceled")
-        }
-      } else {
-        cont.resumeWithException(e)
-      }
-    }
   }
 }
